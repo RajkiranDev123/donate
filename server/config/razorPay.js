36f70ee71a36d52a62bb76bc712d07a6a7e3bc78
raj <rajtech645@gmail.com>
2025-07-04 09:52:19 +0530

    some bug fixes

--- server/config/razorPay.js
+++ server/config/razorPay.js
@@ -4,7 +4,7 @@ dotenv.config({ path: './.env' })
 import razorpay from "razorpay"
 
 
-console.log(process.env.RAZORPAY_KEY_SECRET)
+
 export const createRazorpayInstance = () => {
     return new razorpay({
         key_id: process.env.RAZORPAY_KEY_ID,
